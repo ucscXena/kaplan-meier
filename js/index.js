498153aaa2ecf5f80a5bbc6918185b6221e8b153
Brian Craft <bcboy@thecraftstudio.com>
2015-10-08 11:02:50 -0700

    Port latest math fixes.

--- js/index.js
+++ js/index.js
@@ -202,12 +202,16 @@ function logranktest (allGroupsRes, groupedDataTable) {
 				if (t < OETable[i].timeNumber && t < OETable[j].timeNumber) {
 					Ki = OETable[i].dataByTimeTable[t].n;
 					Kj = OETable[j].dataByTimeTable[t].n;
-					if (i !== j) { // https://books.google.com/books?id=nPkjIEVY-CsC&pg=PA451&lpg=PA451&dq=multivariate+hypergeometric+distribution+covariance&source=bl&ots=yoieGfA4bu&sig=dhRcSYKcYiqLXBPZWOaqzciViMs&hl=en&sa=X&ved=0CEQQ6AEwBmoVChMIkqbU09SuyAIVgimICh0J3w1x#v=onepage&q=multivariate%20hypergeometric%20distribution%20covariance&f=false
+					// https://books.google.com/books?id=nPkjIEVY-CsC&pg=PA451&lpg=PA451&dq=multivariate+hypergeometric+distribution+covariance&source=bl&ots=yoieGfA4bu&sig=dhRcSYKcYiqLXBPZWOaqzciViMs&hl=en&sa=X&ved=0CEQQ6AEwBmoVChMIkqbU09SuyAIVgimICh0J3w1x#v=onepage&q=multivariate%20hypergeometric%20distribution%20covariance&f=false
+					// when N==1: only 1 subject, no variance
+					if (i !== j && N !== 1){
 						vv[i][j] -= n * Ki * Kj * (N - n) / (N * N * (N - 1));
-						vv[j][i] -= n * Ki * Kj * (N - n) / (N * N * (N - 1));
+						vv[j][i] = vv[i][j];
 					}
 					else {//i==j
-						vv[i][i] += n * Ki * (N - Ki) * (N - n) / (N * N * (N - 1));
+						if (N !== 1) {
+							vv[i][i] += n * Ki * (N - Ki) * (N - n) / (N * N * (N - 1));
+						}
 					}
 				}
 			}
